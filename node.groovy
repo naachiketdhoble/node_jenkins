679b6281afb166fbbb300d73fd89c05da8e0aa21
Kartik <lionking@gmail.com>
2026-06-05 16:54:50 +0530

    changes made here

--- node.groovy
+++ node.groovy
@@ -60,7 +60,7 @@ pipeline {
                     usernamePassword(
                         credentialsId: 'dockerhub-creds',
                         usernameVariable: 'DOCKER_USERNAME',
-                        passwordVariable: 'DOCKER_PASSORD'
+                        passwordVariable: 'DOCKER_PASSWORD'
                     )
                 ]) {
                     sh 'docker login -u ${DOCKER_USERNAME} -p ${DOCKER_PASSWORD}'
